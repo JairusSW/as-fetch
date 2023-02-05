054d8fb06cc8b48ecf7df00a567770d051a4d606
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-02-05 14:30:44 -0800

    req and res

---
+++ assembly/src/Request.ts
@@ -0,0 +1,27 @@
+import { Body } from "./Body";
+import { Headers } from "./Headers";
+
+export class RequestInit {
+    method: string | null = null;
+    headers: Array<Array<string>> | null = null;
+    body: ArrayBuffer | null = null;
+    mode: string | null = null;
+    // TODO torch2424 (6/9/20): Credentials
+    // TODO torch2424 (6/9/20): Cache
+    // TODO torch2424 (6/9/20): Redirect
+    // TODO torch2424 (6/9/20): Referrer
+    // TODO torch2424 (6/9/20): Integrity
+}
+
+export class Request extends Body {
+    public method!: string;
+    public url!: string;
+    public headers!: Headers;
+    constructor(url: string, init: RequestInit | null = null) {
+        init?.body ? super(init.body) : super(null);
+        this.url = url;
+        this.method = init?.method || "GET";
+        this.headers = new Headers();
+        // Set headers
+    }
+}
---
+++ assembly/src/Response.ts
@@ -0,0 +1,25 @@
+import { Body } from "./Body";
+import { Status } from "./Status";
+
+export class ResponseInit {
+    status: number = 200;
+    headers: Headers | null = null;
+    url: string | null = null;
+    redirected: boolean = false
+}
+
+export class Response extends Body {
+    public url!: string;
+    public status!: i32;
+    public statusText!: string;
+    public headers!: Headers;
+    public redirected!: boolean;
+    public ok!: boolean;
+    constructor(body: ArrayBuffer | null, init: ResponseInit) {
+        super(body);
+        this.headers = init.headers || new Headers();
+        this.url = init.url || "";
+        this.redirected = init.redirected;
+        this.ok = this.status >= 200 && this.status < 300;
+    }
+}
