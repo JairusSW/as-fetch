44a727dda1f081a240622bbdc7f52fed82b63b9e
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-07-28 12:48:24 -0700

    forgor to save file 💀

--- bindings.raw.d.ts
+++ bindings.raw.d.ts
@@ -1,7 +1,7 @@
 export class FetchHandler {
     public imports: {
-        _fetchGET(url: string, headers: string[][], callbackID: string): void;
-        _fetchPOST(url: string, headers: string[][], body: ArrayBuffer, callbackID: string): void;
+        _fetchGET(url: string, headers: string[][], callbackID: number): void;
+        _fetchPOST(url: string, headers: string[][], body: ArrayBuffer, callbackID: number): void;
     };
     constructor(fetchImpl: any);
     init(exp: WebAssembly.Exports): void;
