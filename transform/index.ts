fdfb6635487aa6900b6c070ed7935037531ff7eb
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-08-10 08:57:49 -0700

    update deps

--- transform/index.ts
+++ transform/index.ts
@@ -2,7 +2,6 @@ import { Transform } from "assemblyscript/dist/transform.js"
 import { Module } from "assemblyscript/dist/assemblyscript";
 export default class Asyncify extends Transform {
     afterCompile(mod: Module) {
-        console.log(mod);
         mod.runPasses(["asyncify"]);
     }
 }
